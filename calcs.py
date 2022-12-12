b95fe4104acdef3dd30429219cb39e0db293dc83
razbiralochka <razbiralochka@yandex.ru>
2022-12-13 01:49:47 +0400

    added RK4 core

--- calcs.py
+++ calcs.py
@@ -5,12 +5,7 @@ import numpy as np
 
 
 
-'''
-0 - radius
-1 - angle
-2 - radial speed
-3 - angular speed
-'''
+
 
 
 
@@ -22,24 +17,32 @@ def core(x,y):
     global alpha
     global acc
 
+    '''
+    0 - radius
+    1 - angle
+    2 - radial speed
+    3 - angular speed
+    4 - mass
+    '''
 
     mu = 398.6e12
     mass = 20000
     thrust = 100
     alpha = 0
     acc = 0
-    vars = np.zeros(4)
-    vars[0] = (6371 + 1000) * 1000
+    vars = np.zeros(5)
+    vars[0] = (6371 + 400) * 1000
     vars[3] = np.sqrt(mu / vars[0])
     t = 0
     h = 100
 
-    k = np.zeros((4, 4))
+    k = np.zeros((5, 4))
 
-    while t < 100:
+    while t < 100*86400:
+        print(vars[0]/1000)
         x.append(vars[0] * np.cos(vars[1])/1000)
         y.append(vars[0] * np.sin(vars[1])/1000)
-        acc = 10*80*0.001/mass
+        acc = 0
 
         k[:, 0] = diffs(vars)
         k[:, 1] = diffs(vars + k[:, 0] / 2)*2
@@ -48,11 +51,11 @@ def core(x,y):
 
         k *= h/6
 
-        dvars = [sum(elem)/6 for elem in k]
+        dvars = [sum(elem) for elem in k]
 
         vars += dvars
 
-        if vars[0] > 42000000:
+        if vars[0] > 42000000 or vars[0] < 0:
             break
 
         t += h
@@ -66,11 +69,12 @@ def diffs(args):
     phi_ = args[1]
     Vr_ = args[2]
     Vphi_ = args[3]
+    mass_ = args[4]
     dr_ = Vr_
     dphi_ =  Vphi_/r_
     dVr_ = (Vphi_ ** 2) / r_ - mu / pow(r_, 2) + np.sin(alpha) * acc
     dVphi_ = -(Vr_*Vphi_)/r_+np.cos(alpha)*acc
-    res = np.array([dr_, dphi_, dVr_, dVphi_])
+    res = np.array([dr_, dphi_, dVr_, dVphi_, mass_])
     return res
 
 

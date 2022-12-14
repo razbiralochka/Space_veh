e61504bf9238d13b7178bde892b8891f9db21573
razbiralochka <razbiralochka@yandex.ru>
2022-12-14 22:26:07 +0400

    added RK4 core via angle

--- calcs.py
+++ calcs.py
@@ -1,74 +1,63 @@
 import numpy as np
 
-
-def core(x,y):
-    global mu
-    global mass
-    global thrust
-    global vars
-    global alpha
-    global acc
-
-    '''
-    0 - radius
-    1 - time
-    2 - radial speed
-    3 - angular speed
-    4 - mass
-    '''
-
-    mu = 398.6e12
-    mass = 20000
-    thrust = 100
-    alpha = np.pi/2
-    acc = 0
-    vars = np.zeros(5)
-    vars[0] = (6371 + 400) * 1000
-    vars[3] = np.sqrt(mu / vars[0])
-    angle = 0
-    h = 5*np.pi/180
-
-    k = np.zeros((5, 4))
-
-    while vars[0] <= 42164000:
-
-        x.append(vars[0]/1000 * np.cos(angle))
-        y.append(vars[0]/1000 * np.sin(angle))
-
-        #acc =  5.4*2 / 20_000
-        acc = 1*10 / 20_000
-        k[:, 0] = diffs(vars)
-        k[:, 1] = diffs(vars + k[:, 0] / 2)*2
-        k[:, 2] = diffs(vars + k[:, 1] / 2)*2
-        k[:, 3] = diffs(vars + k[:, 2])
-
-        k *= h/6
-
-        dvars = [sum(elem) for elem in k]
-
-        vars += dvars
+class calcs_class():
+    def __init__(self, radius, speedAng, speedrad, k_radius, k_speedAng ,k_speedrad):
+        self.vars = np.zeros(5)
+        self.vars[0] = radius
+        self.vars[3] = speedAng
+        self.vars[2] = speedrad
+        self.rk = k_radius
+        self.vars[4] = 0
+        self.vphik = k_speedAng
+        self.vrk = k_speedrad
+        self.x = list()
+        self.y = list()
+    def rungekutta4(self):
+        angle = 0
+        k = np.zeros((5, 4))
+        h = np.pi/180
+        while self.vars[0] < self.rk:
+            self.x.append(self.vars[0] * np.cos(angle))
+            self.y.append(self.vars[0] * np.sin(angle))
 
 
 
-        angle += h
-    print(vars[1]/86400)
-    return x, y
+            k[:, 0] = self.diffs(self.vars)
+            k[:, 1] = self.diffs(self.vars + k[:, 0] / 2) * 2
+            k[:, 2] = self.diffs(self.vars + k[:, 1] / 2) * 2
+            k[:, 3] = self.diffs(self.vars + k[:, 2])
+
+            k *= h / 6
+
+            dvars = [sum(elem) for elem in k]
+
+            self.vars += dvars
+
+            angle += h
+
+        return self.x, self.y
+
+    def diffs(self, args):
+        alpha = np.pi/2
+        acc = 0.01
+        r_ = args[0]
+        time_ = args[1]
+        Vr_ = args[2]
+        Vphi_ = args[3]
+        mass_ = args[4]
+        dr_ = Vr_ * r_ / Vphi_
+        dtime_ = r_ / Vphi_
+        dVr_ = Vphi_ - 1 / (r_ * Vphi_) + np.cos(alpha) * acc * r_ / Vphi_
+        dVphi_ = -Vr_ + np.sin(alpha) * acc * r_ / Vphi_
+        res = np.array([dr_, dtime_, dVr_, dVphi_, mass_])
+
+        return res
 
 
-def diffs(args):
-    r_ = args[0]
-    time_ = args[1]
-    Vr_ = args[2]
-    Vphi_ = args[3]
 
-    mass_ = args[4]
-    dr_ = Vr_*r_/Vphi_
-    dtime_ =  r_/Vphi_
-    dVr_ = Vphi_-mu/(r_*Vphi_)+np.cos(alpha)*acc*r_/Vphi_
-    dVphi_ = -Vr_+np.sin(alpha) * acc * r_/Vphi_
-    res = np.array([dr_, dtime_, dVr_, dVphi_, mass_])
 
-    return res
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from calcs import core
+import numpy as np
+
+from calcs import calcs_class
 import  matplotlib.pyplot as plt
 
 
+r_0 = (6371+200)/42164
+vphi_0 = 1/np.sqrt(r_0)
+vr_0 = 0
+r_k = 1
+v_phi_k= 1
+vr_k = 0
 
 
 
-x = list()
-y = list()
+calcs = calcs_class(r_0, vphi_0, vr_0, r_k, v_phi_k, vr_k)
 
 
-x,y = core(x,y)
+
+
+
+
+x,y = calcs.rungekutta4()
 
 
 plt.plot(x,y)

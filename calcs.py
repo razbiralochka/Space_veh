7440131b566d9fe598ca585343636e7bc376eb75
razbiralochka <razbiralochka@yandex.ru>
2022-12-15 21:31:13 +0400

    micro-refactoring

--- calcs.py
+++ calcs.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class calcs_class():
-    def __init__(self, radius, speedAng, speedrad, k_radius, k_speedAng ,k_speedrad):
+    def __init__(self, acc, radius, speedAng, speedrad, k_radius, k_speedAng ,k_speedrad):
         self.vars = np.zeros(5)
         self.vars[0] = radius
         self.vars[3] = speedAng
@@ -10,36 +10,47 @@ class calcs_class():
         self.vars[4] = 0
         self.vphik = k_speedAng
         self.vrk = k_speedrad
-        self.x = list()
-        self.y = list()
+
+        self.acc = acc
+
     def rungekutta4(self):
+
+        args = np.array([elem for elem in self.vars])
+        print(self.vars)
+        x = list()
+        y = list()
         angle = 0
         k = np.zeros((5, 4))
-        h = np.pi/180
-        while self.vars[0] < self.rk:
-            self.x.append(self.vars[0] * np.cos(angle))
-            self.y.append(self.vars[0] * np.sin(angle))
+        h = 1 * np.pi/180
+        while args[0] < self.rk:
 
+            #h = 0.01/self.args[0]
 
+            y.append(args[0])
+            x.append(angle)
 
-            k[:, 0] = self.diffs(self.vars)
-            k[:, 1] = self.diffs(self.vars + k[:, 0] / 2) * 2
-            k[:, 2] = self.diffs(self.vars + k[:, 1] / 2) * 2
-            k[:, 3] = self.diffs(self.vars + k[:, 2])
+            k[:, 0] = self.diffs(args)
+            k[:, 1] = self.diffs(args + k[:, 0] / 2) * 2
+            k[:, 2] = self.diffs(args + k[:, 1] / 2) * 2
+            k[:, 3] = self.diffs(args + k[:, 2])
 
             k *= h / 6
 
             dvars = [sum(elem) for elem in k]
 
-            self.vars += dvars
+            args += dvars
 
             angle += h
-
-        return self.x, self.y
+        print(angle/(2*np.pi))
+        r0 =6571000
+        mu = 398600.4415*(10**9)
+        time = args[1]*r0*np.sqrt(r0/mu)
+        print(time/86400)
+        return x, y
 
     def diffs(self, args):
         alpha = np.pi/2
-        acc = 0.01
+        acc = self.acc
         r_ = args[0]
         time_ = args[1]
         Vr_ = args[2]
@@ -47,8 +58,8 @@ class calcs_class():
         mass_ = args[4]
         dr_ = Vr_ * r_ / Vphi_
         dtime_ = r_ / Vphi_
-        dVr_ = Vphi_ - 1 / (r_ * Vphi_) + np.cos(alpha) * acc * r_ / Vphi_
-        dVphi_ = -Vr_ + np.sin(alpha) * acc * r_ / Vphi_
+        dVr_ = Vphi_ - 1 / (r_ * Vphi_)
+        dVphi_ = -Vr_ + (r_ / Vphi_)*acc
         res = np.array([dr_, dtime_, dVr_, dVphi_, mass_])
 
         return res
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 from calcs import calcs_class
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
-r_0 = (6371+200)/42164
+r_0 = 1
+
+
+
 vphi_0 = 1/np.sqrt(r_0)
 vr_0 = 0
-r_k = 1
-v_phi_k= 1
+r_k = 42164/(6371+200)
+v_phi_k= 1/np.sqrt(r_k)
 vr_k = 0
+acc = 0.000081328
 
 
 
-calcs = calcs_class(r_0, vphi_0, vr_0, r_k, v_phi_k, vr_k)
+calcs = calcs_class(acc, r_0, vphi_0, vr_0, r_k, v_phi_k, vr_k)
 
 
 
@@ -23,8 +27,8 @@ calcs = calcs_class(r_0, vphi_0, vr_0, r_k, v_phi_k, vr_k)
 x,y = calcs.rungekutta4()
 
 
-plt.plot(x,y)
-plt.axis('equal')
+plt.polar(x, y)
+
 plt.grid()
 plt.show()
 

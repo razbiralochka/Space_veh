7a70a75e94b6779e79f197fb37e384f1a824cd3c
razbiralochka <razbiralochka@yandex.ru>
2022-12-17 18:48:42 +0400

    averaged equatitions

--- calcs.py
+++ calcs.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 class calcs_class():
-    def __init__(self, acc, radius, speedAng, speedrad, k_radius, k_speedAng ,k_speedrad):
-        self.vars = np.zeros(5)
+    def __init__(self, acc, radius):
+        self.vars = np.zeros(6)
         self.vars[0] = radius
-        self.vars[3] = speedAng
-        self.vars[2] = speedrad
-        self.rk = k_radius
+        self.vars[1] = 0
+        self.vars[2] = 0
+        self.vars[3] = 0
         self.vars[4] = 0
-        self.vphik = k_speedAng
-        self.vrk = k_speedrad
-
+        self.vars[5] = 0
         self.acc = acc
+
+    def fit(self):
+        pass
+
 
     def rungekutta4(self):
 
@@ -19,15 +21,16 @@ class calcs_class():
         print(self.vars)
         x = list()
         y = list()
-        angle = 0
-        k = np.zeros((5, 4))
-        h = 2 * np.pi/180
-        while args[0] < self.rk:
+        time = 0
+        k = np.zeros((6, 4))
 
-            #h = 0.01/self.args[0]
+        r_k = 42164 / (6371 + 200)
+        while args[0] < r_k:
+
+            h = 0.1 * args[0]
 
             y.append(args[0])
-            x.append(angle)
+            x.append(args[1])
 
             k[:, 0] = self.diffs(args)
             k[:, 1] = self.diffs(args + k[:, 0] / 2) * 2
@@ -40,29 +43,34 @@ class calcs_class():
 
             args += dvars
 
-            angle += h
-        print(angle/(2*np.pi))
+            time += h
+
         r0 =6571000
         mu = 398600.4415*(10**9)
         time = args[1]*r0*np.sqrt(r0/mu)
-        print(args[1],' ',time/86400)
-        print(args[4])
+        print(time)
         return x, y
 
     def diffs(self, args):
-        alpha = np.pi/2
         acc = self.acc
+
         r_ = args[0]
-        time_ = args[1]
-        Vr_ = args[2]
-        Vphi_ = args[3]
-        mass_ = args[4]
-        dr_ = Vr_ * r_ / Vphi_
-        dtime_ = r_ / Vphi_
-        dVr_ = Vphi_ - 1 / (r_ * Vphi_)
-        dVphi_ = -Vr_ + (r_ / Vphi_)*acc/(1-mass_)
-        dmass_ = (acc/6.4)*(r_/Vphi_)
-        res = np.array([dr_, dtime_, dVr_, dVphi_, dmass_])
+        phi_ = args[1]
+        m_ = args[2]
+
+        pf_ = args[3]
+        pr_ = args[4]
+        pm_ = args[5]
+
+        dr_ = 2*pow(r_,1.5)*acc/(1-m_)
+        dphi_ = 1/pow(r_,1.5)
+        dmass_ = acc/6.4
+
+        dpf_ = 0
+        dpr_ = 1.5*pf_/pow(r_, 2.5)-3*acc*pr_*np.sqrt(r_)/(1-m_)
+        dpm_ = -2*acc*pr_*pow(r_, 1.5)/(1-m_)**2
+
+        res = np.array([dr_, dphi_, dmass_, dpf_, dpr_, dpm_])
 
         return res
 

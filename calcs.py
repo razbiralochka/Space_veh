244e18263d4acc55ff30b87d20eb1aa1f158ae08
razbiralochka <razbiralochka@yandex.ru>
2022-12-18 11:42:14 +0400

    Corrected RK4

--- calcs.py
+++ calcs.py
@@ -2,24 +2,26 @@ import numpy as np
 
 class calcs_class():
     def __init__(self, acc):
+        self.r_k = 42164 / (6371 + 200)
+        self.acc = acc
+        self.mu = 398600.4415 * (10 ** 9)
+
         self.vars = np.zeros(10)
         self.vars[0] = 1 #r
         self.vars[1] = 0 #phi
-        self.vars[2] = np.sqrt(1/self.vars[0])+1 #Vphi
+        self.vars[2] = np.sqrt(1/self.vars[0])#Vphi
         self.vars[3] = 0 #Vr
         self.vars[4] = 0 #m
 
-        self.vars[5] = -0.01  # Pr
-        self.vars[7] = -0.01  # pVr
-        self.vars[8] = -0.01  # pVphi
+        self.vars[5] = -0.000001  # Pr
+        self.vars[7] = 0  # pVr
+        self.vars[8] = 0.000001  # pVphi
 
         self.vars[6] = 0  # Pphi
         self.vars[9] = -1  # Pm
 
 
 
-        self.r_k = 42164/(6371+200)
-        self.acc = acc
 
         self.err = 0
     def fit(self):
@@ -62,19 +64,19 @@ class calcs_class():
         #7099.125838682138
         while args[0] < self.r_k:
 
-
             if time > 8000:
                 break
-            h = 0.1/args[2]
+
+            h = 0.1
             t_list.append(time)
             vr_list.append(args[3])
             r.append(args[0])
             angle.append(args[1])
 
             k[:, 0] = self.diffs(args)
-            k[:, 1] = self.diffs(args + k[:, 0] / 2) * 2
-            k[:, 2] = self.diffs(args + k[:, 1] / 2) * 2
-            k[:, 3] = self.diffs(args + k[:, 2])
+            k[:, 1] = self.diffs(args + k[:, 0] *h/ 2) * 2
+            k[:, 2] = self.diffs(args + k[:, 1] *h/ 2) * 2
+            k[:, 3] = self.diffs(args + k[:, 2]*h)
 
 
             k *= h/6
@@ -89,8 +91,7 @@ class calcs_class():
 
 
         print(time)
-        err = np.sin(args[1])**2+args[2]
-        self.err = err
+
         return angle, r, t_list, vr_list
 
     def diffs(self, args):
@@ -108,29 +109,29 @@ class calcs_class():
 
         flag = pm/6.8 + pvp/(1-m)
 
-        #print(flag)
 
-        acc = 0*self.acc#*(flag > -1)
+
+        acc = self.acc*(flag > 0)
 
 
 
 
         dr = vr
         dp = vp/r
-        dVp = -(vr*vp)/r+0
-        dVr = (vp*vp)/r - 1/(r*r)
+        dVr = vp**2 / r - 1 / r**2
+        dVp = -(vr*vp)/r+acc
         dm = acc/6.8
 
         dPm = -acc*pvp/(1-m)**2
         dPvp = (pvp*vr-2*pvr*vp-pp)/r
         dPvr = pvp*vp/r-pr
         dPr = pp*vp/(r*r)-pvp*vr*vp/(r*r)-pvp*(2/(r*r*r)-vp*vp/(r*r))
+        dpp = 0
 
 
 
 
-
-        res = np.array([dr,dp,dVp,dVr,dm,dPr,0,dPvr,dPvp,dPm])
+        res = np.array([dr,dp,dVp,dVr,dm,dPr,dpp,dPvr,dPvp,dPm])
 
         return res
 

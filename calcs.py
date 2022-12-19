3bd3cbe9405201cc7c61be7df8e778b9c2684ad2
razbiralochka <razbiralochka@yandex.ru>
2022-12-19 19:39:19 +0400

    WORKING

--- calcs.py
+++ calcs.py
@@ -8,30 +8,29 @@ class calcs_class():
 
         self.mu = 398600.4415 * (10 ** 9)
 
-        self.vars = np.zeros(10)
+        self.vars = np.zeros(9)
         self.vars[0] = 1 #r
-        self.vars[1] = 0 #phi
-        self.vars[2] = np.sqrt(1/self.vars[0])#Vphi
-        self.vars[3] = 0 #Vr
-        self.vars[4] = 0 #m
+        self.vars[3] = 1 #Vphi
 
 
 
-        self.vars[6] = 0  # Pphi
-        self.vars[9] = -1 # Pm
+
+
+        self.vars[8] = -1 # Pm
 
 
 
 
         self.err = 0
     def fit(self):
-        a = -1
-        b = 0
-        c = -1
-        h = 0.01
+        a = 1
+        b = 1
+        c = 1
+        h = 0.0001
+        g= 1
         print('fit')
-        for i in range(10):
-
+        for i in range(1000):
+            err = 100
             da = (self.rungekutta4(a + h, b, c) - self.rungekutta4(a - h, b, c)) / (2 * h)
             db = (self.rungekutta4(a, b + h, c) - self.rungekutta4(a, b - h, c)) / (2 * h)
             dc = (self.rungekutta4(a, b, c + h) - self.rungekutta4(a, b, c - h)) / (2 * h)
@@ -39,12 +38,19 @@ class calcs_class():
 
 
 
-            a = a - 0.01 * da
-            b = b - 0.01 * db
-            c = c - 0.01 * dc
+            a = a - g * da
+            b = b - g * db
+            c = c - g * dc
+
+            if self.err < err:
+                err = self.err
+
+            if self.err > err:
+                h*=0.99
+                g*=0.5
 
             print('iter: #',i+1, 'err', self.err)
-            print('Pr: ',a,'   Pvr: ',b,'   Pv_phi: ',c)
+            print('Pr: ',a ,'   Pvr: ',b,'   Pv_phi: ',c)
 
 
 
@@ -57,20 +63,21 @@ class calcs_class():
 
         args = np.array([elem for elem in self.vars])
         args[5] = a
-        args[7] = b
-        args[8] = c
+        args[6] = b
+        args[7] = c
         r = list()
         angle = list()
         t_list = list()
         vr_list = list()
 
         time = 0
-        k = np.zeros((10,4))
+        k = np.zeros((9, 4))
 
 
 
         #7099.125838682138
-        while time < 10000:
+        while time < 8000:
+
 
 
             h = 2*np.pi/100
@@ -83,6 +90,9 @@ class calcs_class():
             k[:, 1] = self.diffs(args + k[:, 0] * h / 2) * 2
             k[:, 2] = self.diffs(args + k[:, 1] * h / 2) * 2
             k[:, 3] = self.diffs(args + k[:, 2] * h)
+
+
+
             k *= h / 6
 
             dvars = np.array([sum(elem) for elem in k])
@@ -100,44 +110,39 @@ class calcs_class():
         return err
 
     def diffs(self, args):
-        r = args[0]
-        p = args[1]
-        vp = args[2]
-        vr = args[3]
-        m = args[4]
-        pr = args[5]
-        pp = args[6]*0
-        pvr = args[7]
-        pvp = args[8]
-        pm = args[9]
+        r_ = args[0]
+        vr_ = args[2]
+        vp_ = args[3]
+        m_ = args[4]
+        pr_ = args[5]
+        pvr_ = args[6]
+        pvp_ = args[7]
+        pm_ = args[8]
 
 
-        flag = pm/6.8 + pvp/(1-m)
-
-
-
+        flag = pm_/6.8 + pvp_/(1-m_)
 
         acc = self.acc*(flag > 0)
 
-
-
-
-
-        dr = vr
-        dp = vp/r
-        dVr = vp**2 / r - 1 / r**2
-        dVp = -(vr*vp)/r+acc
+        dr = vr_
+        dp = vp_/r_
+        dVr = vp_**2 / r_ - 1 / r_**2
+        dVp = -(vr_*vp_)/r_+acc/(1-m_)
         dm = acc/6.8
 
-        dPm = -acc*pvp/(1-m)**2
-        dPvp = (pvp*vr-2*pvr*vp-pp)/r
-        dPvr = pvp*vp/r-pr
-        dPr = pp*vp/(r*r)-pvp*vr*vp/(r*r)-pvp*(2/(r*r*r)-vp*vp/(r*r))
-        dpp = 0
+        dPm = -acc*pvp_/(1-m_)**2
+        dPvp = -(2*pvr_*vp_)/r_ + (pvp_*vr_)/r_
+        dPvr = pvp_*vp_/r_-pr_
+        dPr = -(pvp_*vr_*vp_)/pow(r_,2)-pvr_*(2/pow(r_,3)-pow(vp_,2)/pow(r_,2))
 
 
 
 
-        res = np.array([dr, dp, dVp, dVr, dm, dPr, dpp, dPvr, dPvp, dPm])
+
+
+
+
+
+        res = np.array([dr, dp, dVr, dVp, dm, dPr, dPvr,dPvp,dPm])
 
         return res
